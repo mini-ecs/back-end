e0012a9da99edfd02688c7295a3d219c3ceb8073
lincyawer <53161583+Lincyaw@users.noreply.github.com>
2022-06-01 11:12:18 +0800

    修改readme

--- README.md
+++ README.md
@@ -12,6 +12,10 @@
 
 [参考文件](https://github.com/golang-standards/project-layout) 。
 
-## [swagger接口](https://github.com/swaggo/swag/blob/master/README_zh-CN.md)
+`api`文件夹中的代码用于处理前端发送过来的restful请求，请求的路由文件位于`internal/router`目录下。原理是在项目初始化时，将这些api处理函数注册到gin的路由内，
+路由管理器在接收到对应的请求时，会调用已注册的函数来响应该请求。
 
-在项目根目录运行`sh build/build.sh`即可生成接口文档。
+
+## 备注
+
+因项目开发时间有限，部分测试代码仅用于实验对应的接口是否可行。
--- internal/image_manager/manager.go
+++ internal/image_manager/manager.go
@@ -1,17 +1,19 @@
 package image_manager
 
 import (
-	"github.com/mini-ecs/back-end/pkg/log"
-	"io"
-	"os"
+    "github.com/mini-ecs/back-end/pkg/log"
+    "io"
+    "os"
 )
 
 // https://github.com/willscott/go-nfs-client
 // https://github.com/willscott/go-nfs
+// 镜像管理可以使用不同的实现方式，这里只实现了本地文件系统的方式
+// 也可以使用网络文件系统、对象存储等其他的方式
 
 type ImageManager interface {
-	Copy(new, old string) error
-	Delete(image string) error
+    Copy(new, old string) error
+    Delete(image string) error
 }
 
 var LocalMachineImpl ImageManager = &localMachineImpl{}
